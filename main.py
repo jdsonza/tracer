099083478ed3596d62322b491a2efd947f8ec54d
Jarryd Son <jdsonza@gmail.com>
2021-03-04 00:53:15 +0200

    add labels for user input

--- main.py
+++ main.py
@@ -8,10 +8,14 @@ from pandastable import Table, TableModel
 
 
 class Window:
-    def __init__(self, root):
+    def __init__(self, root, n_prior_days):
         self.root = root
         root.title("Contact Tracing")
         root.geometry()
+        self.selected_files_label = tk.Label(
+            master=root,
+            text="Select course register sheets"
+        )
         self.browse_button = tk.Button(
             master=root, 
             text="Browse files", 
@@ -25,42 +29,75 @@ class Window:
         self.positive_attendee_id_entry = tk.Entry(
             master=root
         )
+        self.positive_attendee_label = tk.Label(
+            root, 
+            text='Student ID/Staff number of positive member'
+        )
         self.positive_attendee_id_entry.insert(0, 'INFECT001')
+        self.positive_confirmed_date_label = tk.Label(
+            root, 
+            text='Date of confirmed infection'
+        )
         self.positive_confirmed_date = DateEntry(master=root)
+        self.filter_unique_var = tk.IntVar(value=1)
+        self.filter_unique_button = tk.Checkbutton(root,
+            text='Show unique contacts', variable=self.filter_unique_var)
+        
+        self.selected_files_label.grid(row=1, column=1)
+        self.browse_button.grid(row=1, column=2)
+        self.positive_attendee_label.grid(row=2, column=1)
+        self.positive_attendee_id_entry.grid(row=2, column=2)
+        self.positive_confirmed_date_label.grid(row=3, column=1)
+        self.positive_confirmed_date.grid(row=3, column=2)
+        self.filter_unique_button.grid(row=4, column=1)
+        self.execute_button.grid(row=5, column=1)
 
+        self.n_prior_days = n_prior_days
 
-        self.browse_button.pack()
-        self.positive_attendee_id_entry.pack()
-        self.positive_confirmed_date.pack()
-        self.execute_button.pack()
-    
     def browse_files(self):
         self.filenames = tk.filedialog.askopenfilenames(
             initialdir = str(Path.cwd()),
-            title = "Select a File",
+            title = "Select Files",
             filetypes = (("Excel Files",
             ".xlsx .xls"),
             ("all files",
             "*.*"))
         )
+        short_names = []
+        for filename in self.filenames:
+            short_names.append(str(Path(filename).name))
+
+        self.selected_files_label.config(text=short_names)
     
     def contact_tracing(self):
         self.df = read_excel(self.filenames)
         self.df = data_preprocessing(self.df)
         positive_attendee_id = self.positive_attendee_id_entry.get() 
         positive_attendee_appointments = self.df.query(
-            "Attendees_User_ID == @positiveAttendeeID"
+            "Attendees_User_ID == @positive_attendee_id"
         )
         contact_conditions = positive_attendee_appointments.filter(
             items=['Appointment_Time', 'Location']
         )
+        all_contact_events = self.df[
+                        self.df['Appointment_Time'].isin(contact_conditions.values[:, 0]) 
+                        & self.df['Location'].isin(contact_conditions.values[:, 1])
+        ]
 
-        self.display_table(positive_attendee_appointments)
+        contact_trace_start_date = pd.Timestamp(self.positive_confirmed_date.get_date()) - pd.Timedelta(self.n_prior_days)
+        recent_contact_events = all_contact_events[all_contact_events.Appointment_Time 
+                                                   > contact_trace_start_date
+        ]
+        
+        if self.filter_unique_var.get():
+            recent_contact_events = recent_contact_events.drop_duplicates(subset=["Attendees_User_ID"])
+
+        self.display_table(recent_contact_events)
     
     
     def display_table(self, df):
         f = tk.Frame(self.root)
-        f.pack()
+        f.grid(row=6, column=1)
         self.table = pt = Table(f, dataframe=df,
             showtoolbar=True, showstatusbar=True
         )
@@ -86,7 +123,7 @@ def data_preprocessing(df):
 
 if __name__ == "__main__":
     root = tk.Tk()   
-    Window(root)
+    Window(root, n_prior_days=14)
     root.mainloop()                                                                                              
 
 
