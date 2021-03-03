dfb45f8cdbe49e5d29051bfbefb19f3815a6f9d4
Jarryd Son <jdsonza@gmail.com>
2021-03-03 12:50:06 +0200

    added filtering for contact tracing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd 
 from pathlib import Path
 
-
-pd.read_excel()
+class_register_dir = Path.cwd() / 'class-registers'
+class_register = pd.read_excel(class_register_dir / 'EEE2045F_all.xlsx')
+print(class_register.
